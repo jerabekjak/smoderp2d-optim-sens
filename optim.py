7b45b180cf2e22d5fa936b1ac6f6268d97019f7c
jerabekjak <jerabekjak@email.cz>
2019-01-14 16:53:17 +0100

    create parser

--- optim.py
+++ optim.py
@@ -1,8 +1,34 @@
 #!/usr/bin/python3
 
-from diff_evol.agent_def import Agent
+import argparse
+
+def main(pars):
+    
+    pass
+    
+
+
+def init() :
+    parser = argparse.ArgumentParser(description='Run Smoderp2D.')
+
+    # type of computation
+    parser.add_argument(
+        '-obs_data',
+        help='location of observed data',
+        type=str,
+        required=True
+    )
+
+    # data file (only required for runoff)
+    parser.add_argument(
+        '-outputdir',
+        help='directory to store the ',
+        type=str,
+        required=True
+    )
+    return parser.parse_args()
 
 if __name__ == '__main__' : 
     
-    a = Agent(3)
-    print (a._pos)
+    
+    main (init())
