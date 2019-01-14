fbd0fefeb08285915f667b0f7e84b591a710debb
jerabekjak <jerabekjak@email.cz>
2019-01-14 20:01:10 +0100

    autopep8

--- exceptions.py
+++ exceptions.py
@@ -2,14 +2,13 @@ class Error(Exception):
     pass
 
 
-
 class IncorrectDataInObsFile(Error):
 
     """Exception raised if the water level goes to negative values.
 
     """
 
-    def __init__(self,val):
+    def __init__(self, val):
         self.msg = 'Incorrect observation data: '+str(val)
 
     def __str__(self):
--- optim.py
+++ optim.py
@@ -3,13 +3,23 @@
 import argparse
 
 from diff_evol.obs_data_handler import ObsData
+from diff_evol import DiffEvol
+from philip_optim import get_ks_s
+
 
 def main(pars):
-    
+
     # load observation data
     OD = ObsData(pars.obs_data)
-    
-def init() :
+
+    # optimaze philips infiltratoin
+    # stores ks and s into .philip file
+    get_ks_s(OD.data)
+
+    DE = DiffEvol(OD.data)
+
+
+def init():
     parser = argparse.ArgumentParser(description='Run Smoderp2D.')
 
     parser.add_argument(
@@ -27,7 +37,7 @@ def init() :
     )
     return parser.parse_args()
 
-if __name__ == '__main__' : 
-    
-    
-    main (init())
+
+if __name__ == '__main__':
+
+    main(init())
