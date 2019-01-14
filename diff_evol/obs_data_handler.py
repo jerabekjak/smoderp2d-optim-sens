8043425d2782c5c5803817a4e337ecac9e443105
jerabekjak <jerabekjak@email.cz>
2019-01-14 19:02:53 +0100

    calculate infiltration from obs data

--- diff_evol/obs_data_handler.py
+++ diff_evol/obs_data_handler.py
@@ -1,19 +1,27 @@
 from exceptions import IncorrectDataInObsFile
 import numpy as np
 
+
 class RecObsData(object):
     """ RecObsData contains data of one observed record """
 
     def __init__(self, n):
 
-        self.time = np.zeros(n,float)
-        self.val = np.zeros(n,float)
-        
-    def set_vals(self,i,time,val):
-        
+        self.time = np.zeros(n, float)
+        self.val = np.zeros(n, float)
+        self.infilt = np.zeros(n, float)
+        self._n = n
+
+    def set_vals(self, i, time, val):
+
         self.time[i] = time
-        self.val[i]  = val
-    
+        self.val[i] = val
+
+    def calc_infiltration(self, rainfall):
+        """ calculate infiltration based on the water level of runoff """
+
+        self.infilt = rainfall - self.val
+
 
 class ObsData(object):
     """ ObsData handle obs data reading 
@@ -25,18 +33,20 @@ class ObsData(object):
 
         ob_f = open(obs_path, 'r')
         ob_ls = ob_f.readlines()
-        
+
         i = 0
         n_read = False
-
-        self.data = []
+        intensit_read = False
 
         for line in ob_ls:
             if ('#' in line[0:4]):
                 continue
             else:
-                if (not(n_read)):
-                    self.n = self._read_line_n(line)
+                if (not(n_read) and not(intensit_read)):
+                    self.rainfall = self._read_line_float(line)
+                    intensit_read = True
+                elif (not(n_read) and intensit_read):
+                    self.n = self._read_line_int(line)
                     self.data = RecObsData(self.n)
                     n_read = True
                 else:
@@ -45,9 +55,18 @@ class ObsData(object):
                     self.data.set_vals(i=i, time=time, val=val)
                     i += 1
 
+        self.data.calc_infiltration(self.rainfall)
+
         ob_f.close()
 
-    def _read_line_n(self, _line):
+    def _read_line_int(self, _line):
+        try:
+            val = int(_line)
+        except ValueError:
+            raise IncorrectDataInObsFile(_line)
+        return val
+
+    def _read_line_float(self, _line):
         try:
             val = int(_line)
         except ValueError:
