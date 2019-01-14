354b9e87817b2e7dba25d141694c2c836372887a
jerabekjak <jerabekjak@email.cz>
2019-01-14 19:59:46 +0100

    init the de main class

---
+++ diff_evol/__init__.py
@@ -0,0 +1,18 @@
+from scipy.optimize import differential_evolution
+
+
+class DiffEvol(object):
+
+    def __init__(self, obs):
+        """ init diff evol 
+        1 tell the model which times to store
+        2 prepare the model config
+        3 fo the optimizatoin (see https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.differential_evolution.html)
+
+        :param obs: list of obs_data_handler.RecObsData instances  
+        """
+
+        self._obs = obs
+        self._de = differential_evolution
+
+        pass
