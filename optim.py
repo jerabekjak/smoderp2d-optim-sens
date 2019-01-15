475af488b00aa6be3617e99a2af007dcede06093
jerabekjak <jerabekjak@email.cz>
2019-01-15 15:16:56 +0100

    make out_dir

--- optim.py
+++ optim.py
@@ -4,16 +4,20 @@ from diff_evol.obs_data_handler import ObsData
 from diff_evol import DiffEvol
 from philip_optim import get_ks_s
 from tools import read_parser
+import os
 
 
 def main(pars):
 
     # load observation data
     OD = ObsData(pars.obs_data)
+    
+    if not os.path.exists(pars.out_dir):
+        os.makedirs(pars.out_dir)
 
     # optimaze philips infiltratoin
     # stores ks and s into .philip file
-    get_ks_s(OD.data)
+    get_ks_s(OD.data, pars.out_dir)
 
     DE = DiffEvol(pars = pars, obs = OD)
     print (DE.model([1,1,1]))
