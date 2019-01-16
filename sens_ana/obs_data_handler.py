d46e823d5f262b85fb740dacb733e693b14350e8
jerabekjak <jerabekjak@email.cz>
2019-01-16 14:54:09 +0100

    define and parse scenarios for sensitivity analysis #6

--- sens_ana/obs_data_handler.py
+++ sens_ana/obs_data_handler.py
@@ -13,3 +13,37 @@ class ObsData(DEObsData):
     def __init__(self, config_path):
 
         DEObsData.__init__(self, config_path)
+
+        self._n_scenarios = self._config.getint('SensAna', 'n_scenarios')
+        scenario_def = self._config.get('SensAna', 'scenario_def')
+        self.scenario = [None]*self._n_scenarios
+
+        i = 0
+        with open(scenario_def, 'r') as sc:
+            sc_ls = sc.readlines()
+            for line in sc_ls:
+                vals = self._read_line_vals_sc(line)
+                self.scenario[i] = vals
+                i += 1
+
+    def _read_line_vals_sc(self, _line):
+
+        _line = _line.replace('\n', '').split('\t')
+        if not(len(_line) == 3):
+            raise IncorrectDataInObsFile(_line)
+
+        try:
+            val = list(_line)
+        except ValueError:
+            raise IncorrectDataInObsFile(_line)
+
+        val_out = val
+        i = 0
+        for ival in val:
+            try:
+                val_out[i] = float(ival)
+                i += 1
+            except ValueError:
+                raise IncorrectDataInObsFile(ival)
+
+        return val_out
