0fbc423d9ee3ccd11a4ac4b203e52ef023d21777
jerabekjak <jerabekjak@email.cz>
2019-01-23 09:23:20 +0100

    clear main and parser #14

--- sens.py
+++ sens.py
@@ -1,24 +1,17 @@
 #!/usr/bin/python3
 
-from sens_ana.obs_data_handler import ObsData
-from philip_optim import get_ks_s
 from sens_ana.read_parser import read_parser
 from sens_ana import SensAna
 import os
 
 
 def main(pars):
+    """ Perform one at the time optimization based on Morris, 1991 """
 
-    # load observation data
-    OD = ObsData(pars.sens_conf)
     if not os.path.exists(pars.out_dir):
         os.makedirs(pars.out_dir)
 
-    # optimaze philips infiltratoin
-    # stores ks and s into .philip file
-    get_ks_s(OD.data, pars.out_dir)
-
-    SA = SensAna(pars=pars, obs=OD)
+    SA = SensAna(pars=pars)
     SA.do_sa()
 
 
--- sens_ana/read_parser.py
+++ sens_ana/read_parser.py
@@ -6,27 +6,9 @@ def read_parser():
     parser = argparse.ArgumentParser(
         formatter_class=argparse.RawDescriptionHelpFormatter,
         description=textwrap.dedent('''\
-         Sensitivity analysis of smoderp2d 
-         -----------------------------------------------
+         Sensitivity analysis of smoderp2d based on Morris 1991
+         ------------------------------------------------------
          '''))
-
-    parser.add_argument(
-        '-o',
-        '--out_dir',
-        help='directory to store the results',
-        type=str,
-        default='out-test-sens',
-        required=False
-    )
-
-    parser.add_argument(
-        '-m',
-        '--mod_conf',
-        help='location of model config file',
-        type=str,
-        default='model/test.ini',
-        required=False
-    )
 
     parser.add_argument(
         '-O',
@@ -34,6 +16,15 @@ def read_parser():
         help='location of sens. analysis config file',
         type=str,
         default='sens.cfg',
+        required=False
+    )
+
+    parser.add_argument(
+        '-o',
+        '--out_dir',
+        help='directory to store the results',
+        type=str,
+        default='out-test-sens',
         required=False
     )
 
