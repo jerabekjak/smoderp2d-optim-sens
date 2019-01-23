5b58904306b8983c56af5b3ace4a7317ad2deea1
jerabekjak <jerabekjak@email.cz>
2019-01-23 13:42:35 +0100

    add delta value  #14

--- sens_ana/__init__.py
+++ sens_ana/__init__.py
@@ -2,7 +2,9 @@ from diff_evol import DiffEvol
 
 from diff_evol.mod_data_handling import read_mod_file
 from diff_evol.mod_data_handling import interpolate
+from diff_evol.mod_data_handling import RecModData
 from tools.plots import plot_sa
+import model.smoderp2d.main as sm
 
 import os
 import numpy as np
@@ -20,9 +22,10 @@ class SensAna(DiffEvol):
 
         self._mod_data = None
         self._mod_data_interp = None
+        self._cfgs = cfgs
         self._mod_conf = pars.mod_conf
-        self._mod_file = cfgs.model_file
-
+        self._mod_file = self._cfgs.model_file
+        
         self._out_dir = pars.out_dir
 
         self._read_mod_file = read_mod_file
@@ -34,9 +37,22 @@ class SensAna(DiffEvol):
         self._gen_param_sets(cfgs)
         # base scenarios matrix
         self._B = self._make_base_array(cfgs)
+        # array of elementary effects
+        self._E = np.zeros_like(self._B)
+        self._delta = 1.-1./(self._cfgs.p-1.)
+        
+        # mask of time series
+        self._mask_data = self._make_mod_mask_data()
 
-        self._delta = 1.-1./(cfgs.p-1.)
-
+    def _make_mod_mask_data(self):
+        """ Creates mas for model data interpolation """
+        
+        mask = RecModData(30)
+        time = np.linspace(60., 1800, num=30)
+        val  = np.zeros_like(time)
+        mask.set_vals(time=time,val=val)
+        return (mask)
+        
     def _make_base_array(self, cfgs):
         """ Creates matrix of base scenarios.
 
@@ -44,8 +60,8 @@ class SensAna(DiffEvol):
         nrows = number of replications
         """
 
-        B = np.zeros([cfgs.R, cfgs.k], float)
-        for i in range(cfgs.R):
+        B = np.zeros([self._cfgs.R, self._cfgs.k], float)
+        for i in range(self._cfgs.R):
             params = self._get_param_set(cfgs)
             B[i][:] = params
 
@@ -58,40 +74,70 @@ class SensAna(DiffEvol):
         Ks and S are generated basen on mean and sd of exponent normal distribution.
         """
 
-        self._X_levels = [uniform(cfgs.X[0], cfgs.X[1])
-                          for p in range(0, cfgs.p)]
-        self._Y_levels = [uniform(cfgs.Y[0], cfgs.Y[1])
-                          for p in range(0, cfgs.p)]
-        self._b_levels = [uniform(cfgs.b[0], cfgs.b[1])
-                          for p in range(0, cfgs.p)]
-        self._ret_levels = [uniform(cfgs.ret[0], cfgs.ret[1])
-                            for p in range(0, cfgs.p)]
+        self._X_levels = [uniform(self._cfgs.X[0], self._cfgs.X[1])
+                          for p in range(0, self._cfgs.p)]
+        self._Y_levels = [uniform(self._cfgs.Y[0], self._cfgs.Y[1])
+                          for p in range(0, self._cfgs.p)]
+        self._b_levels = [uniform(self._cfgs.b[0], self._cfgs.b[1])
+                          for p in range(0, self._cfgs.p)]
+        self._ret_levels = [uniform(self._cfgs.ret[0], self._cfgs.ret[1])
+                            for p in range(0, self._cfgs.p)]
 
-        self._Ks_levels = 10.**np.random.normal(cfgs.Ks[0], cfgs.Ks[1], cfgs.p)
-        self._S_levels = 10.**np.random.normal(cfgs.S[0], cfgs.S[1], cfgs.p)
+        self._Ks_levels = 10.**np.random.normal(self._cfgs.Ks[0], self._cfgs.Ks[1], self._cfgs.p)
+        self._S_levels = 10.**np.random.normal(self._cfgs.S[0], self._cfgs.S[1], self._cfgs.p)
 
     def _get_param_set(self, cfgs):
 
-        params = np.zeros([cfgs.k], float)
+        params = np.zeros([self._cfgs.k], float)
 
-        i = randint(0, cfgs.p-1)
+        i = randint(0, self._cfgs.p-1)
         params[0] = self._X_levels[i]
-        i = randint(0, cfgs.p-1)
+        i = randint(0, self._cfgs.p-1)
         params[1] = self._Y_levels[i]
-        i = randint(0, cfgs.p-1)
+        i = randint(0, self._cfgs.p-1)
         params[2] = self._b_levels[i]
-        i = randint(0, cfgs.p-1)
+        i = randint(0, self._cfgs.p-1)
         params[3] = self._Ks_levels[i]
-        i = randint(0, cfgs.p-1)
+        i = randint(0, self._cfgs.p-1)
         params[4] = self._S_levels[i]
-        i = randint(0, cfgs.p-1)
+        i = randint(0, self._cfgs.p-1)
         params[5] = self._ret_levels[i]
 
         return (params)
 
-    def do_sa(self):
+    def _single_el_effect(self,irep,ipar):
+        """ calculates elementary effect of a single ipar parameter in irep replication """
+        
+        # base scenario
+        par_0 = self._B[irep][:]
+        
+        # changed scenario
+        par_d = par_0.copy()
+        par_d[ipar] = par_d[ipar] + self._delta
+        
+        ss_0 = self.model(par_0)
+        ss_d = self.model(par_d)
+        
+        print (ss_0)
+        print (ss_1)
+            
+        
+    def model(self, params):
+        
+        sm.run(self._mod_conf, params, self._cfgs)
+        
+        mod_data = self._read_mod_file(self._mod_file)
+        mod_interp = self._interp_mod_data(mod=mod_data, obs=self._mask_data)
+        print (mod_interp.val)
+        
+    
+    def do_sa(self,cfgs):
 
-        pass
+        for irep in range(self._cfgs.R):
+            for ipar in range(self._cfgs.k):
+                el_effect = self._single_el_effect(irep,ipar)
+                #self._E[irep][ipar] = el_effect
+            
 
     def __del__(self):
         path = '{}{sep}base_scen_array'.format(self._out_dir, sep=os.sep)
