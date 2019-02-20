971bd806fa35a74261b7bfee0fb94216afb76610
jerabekjak <jerabekjaku@gmail.com>
2019-02-20 14:25:31 +0100

    add ret in out/params.dat header #19

--- tools/writes.py
+++ tools/writes.py
@@ -7,7 +7,7 @@ def write_de(obs, mod, result, out_dir):
     path_val = '{0}{sep}obs_mod.dat'.format(out_dir, sep=os.sep)
 
     with open(path_params, 'w') as pf:
-        pf.write('X;Y;b;Ks;S;slope;rainfall;SofSq\n')
+        pf.write('X;Y;b;Ks;S;ret;slope;rainfall;SofSq\n')
         for ix in result.x:
             pf.write('{:1.3e};'.format(ix))
         pf.write('{:1.3e};'.format(obs.slope))
