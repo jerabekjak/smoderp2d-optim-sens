b38c4bb55f25d645c1638080688dc4001017c041
jerabekjak <jerabekjaku@gmail.com>
2019-03-07 12:58:04 +0100

    no comment in header of sens outputs

--- tools/writes.py
+++ tools/writes.py
@@ -31,5 +31,5 @@ def write_sa(res, file_ ,out_dir):
 
     path_params = '{0}{sep}{1}'.format(out_dir, file_, sep=os.sep)
 
-    np.savetxt(path_params, res, fmt='%1.4e',
+    np.savetxt(path_params, res, fmt='%1.4e',comments='',
                header='X;Y;b;Ks;S;ret;SofSq;NashSutcliffe', delimiter=';')
