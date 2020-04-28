a2d5799a6a478a87056afc4e91bf34359dc0cf40
jerabekjak <jerabekjaku@gmail.com>
2020-04-28 10:06:28 +0200

    return old main.py #30 needed for communucation with the rest of the scripts

--- model/smoderp2d/providers/cmd/__init__.py
+++ model/smoderp2d/providers/cmd/__init__.py
@@ -1,42 +1,99 @@
+import os
 import sys
 import argparse
-if sys.version_info > (3, 0):
-    from configparser import ConfigParser
+import logging
+import numpy as np
+if sys.version_info.major >= 3:
+    from configparser import ConfigParser, NoSectionError
 else:
-    from ConfigParser import ConfigParser
+    from ConfigParser import ConfigParser, NoSectionError
 
-from model.smoderp2d.core.general import Globals, GridGlobals
-from model.smoderp2d.providers.base import BaseProvider, Logger
-from argparse import Namespace
+from smoderp2d.core.general import Globals
+from smoderp2d.providers.base import BaseProvider, Logger, CompType, BaseWritter
+from smoderp2d.exceptions import ConfigError
 
+class CmdWritter(BaseWritter):
+    def __init__(self):
+        super(CmdWritter, self).__init__()
+
+    def write_raster(self, array, output_name, directory='core'):
+        """Write raster (numpy array) to ASCII file.
+
+        :param array: numpy array
+        :param output_name: output filename
+        :param directory: directory where to write output file
+        """
+        file_output = self._raster_output_path(output_name, directory)
+
+        np.savetxt(file_output, array, fmt='%.6e')
+
+        self._print_array_stats(
+            array, file_output
+        )
 
 class CmdProvider(BaseProvider):
-    def __init__(self, indata_path):
+    def __init__(self):
         """Create argument parser."""
         super(CmdProvider, self).__init__()
+        
+        # define CLI parser
+        parser = argparse.ArgumentParser(description='Run Smoderp2D.')
 
-        # create parser "by hard"
-        self._args = Namespace(indata=indata_path, typecomp='roff')
+        # type of computation
+        parser.add_argument(
+            '--typecomp',
+            help='type of computation',
+            type=str,
+            choices=['full',
+                     'dpre',
+                     'roff'],
+            required=True
+        )
+
+        # data file (only required for runoff)
+        parser.add_argument(
+            '--indata',
+            help='file with prepared data',
+            type=str
+        )
+        self.args = parser.parse_args()
+        self.args.typecomp = CompType()[self.args.typecomp]
 
         # load configuration
         self._config = ConfigParser()
-        if self._args.typecomp == 'roff':
-            if not self._args.indata:
+        if self.args.typecomp == CompType.roff:
+            if not self.args.indata:
                 parser.error('--indata required')
-            self._config.read(self._args.indata)
+            if not os.path.exists(self.args.indata):
+                raise ConfigError("{} does not exist".format(
+                    self.args.indata
+                ))
+            self._config.read(self.args.indata)
 
-        # set logging level
-        Logger.setLevel(self._config.get('Other', 'logging'))
+        try:
+            # set logging level
+            Logger.setLevel(self._config.get('Other', 'logging'))
+            # sys.stderr logging
+            self._add_logging_handler(
+                logging.StreamHandler(stream=sys.stderr)
+            )
 
-        # must be defined for _cleanup() method
-        Globals.outdir = self._config.get('Other', 'outdir')
+            # must be defined for _cleanup() method
+            Globals.outdir = self._config.get('Other', 'outdir')
+        except NoSectionError as e:
+            raise ConfigError('Config file {}: {}'.format(
+                self.args.indata, e
+            ))
 
-    def load(self, philip, params, obs):
+        # define storage writter
+        self.storage = CmdWritter()
+
+    def load(self):
         """Load configuration data.
 
         Only roff procedure supported.
         """
-        if self._args.typecomp == 'roff':
+        if self.args.typecomp == CompType.roff:
             # cleanup output directory first
             self._cleanup()
 
@@ -45,90 +102,7 @@ class CmdProvider(BaseProvider):
             )
 
             self._set_globals(data)
-
-            #
-            #
-            #
-            #
-            # Change vals in globs for optimalization
-            #self._adjust_domain_size()
-            self._set_philips_to_glob(params)
-            self._set_rainfall_to_glob(obs.rainfall)
-            self._set_slope_to_glob(obs.slope)
-            self._set_optim_params_to_glob(params,obs.slope)
-            self._set_total_raster_area()
-            self._set_surface_retention(params)
-            #
-            #
-            #
-            #
-            #
-            #
         else:
             raise ProviderError('Unsupported partial computing: {}'.format(
-                self._args.typecomp
+                self.args.typecomp
             ))
-
-    def _set_surface_retention(self,params):
-        Globals.mat_reten.fill(params[5])
-
-    def _set_point_loc(self,ii,jj):
-        """ set location of point000.dat to the bottom of the plot """
-        Globals.array_points[0][1] = jj
-        Globals.array_points[0][2] = ii
-            
-            
-    def _set_philips_to_glob(self, params):
-        """ read philip paramaters from hidden file """
-        ks = params[3]
-        s  = params[4]
-            
-        for l in Globals.combinatIndex:
-            l[1] = ks
-            l[2] = s
-            
-    def _set_philips_to_glob_from_file(self, philip):
-        """ read philip paramaters from hidden file """
-        with open(philip, 'r') as pf:
-            lines = pf.readlines()
-            s = float(lines[1].replace('\n', ''))
-            ks = float(lines[2].replace('\n', ''))
-
-        for l in Globals.combinatIndex:
-            l[1] = ks
-            l[2] = s
-
-    def _set_rainfall_to_glob(self, rainfall):
-        """ change rainfall intensity in globals.sr """
-        for l in Globals.sr:
-            l[0] = 3600
-            l[1] = rainfall
-
-    def _set_slope_to_glob(self,slope):
-        """ change surface slope in globals.mat_slope """
-        Globals.mat_slope.fill(slope)
-        
-    def _set_optim_params_to_glob(self,params,slope):
-        """ change surface slope in globals.mat_aa a globals.mat_aa """
-        X = params[0]
-        Y = params[1]
-        b = params[2]
-        Globals.mat_aa = X*Globals.mat_slope**Y
-        Globals.mat_b.fill(b)
-        
-    def _adjust_domain_size(self):
-        """ set domain size to lab rainfal simulator plot """
-        GridGlobals.dx =0.9/4.
-        GridGlobals.dy =8.0/13.
-        GridGlobals.pixel_area =  0.9*8.0
-        
-    def _set_total_raster_area(self):
-        rr = GridGlobals.rr
-        rc = GridGlobals.rc
-        A = 0.0
-        for i in rr :
-            for j in rc[i]:
-                A += GridGlobals.pixel_area
-        GridGlobals.domain_area = A
-            
-
