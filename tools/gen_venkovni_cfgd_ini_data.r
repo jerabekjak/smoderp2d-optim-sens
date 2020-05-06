a6fe3192ae30c8f14ff3bc9d73ca34c363083e62
jerabekjak <jerabekjaku@gmail.com>
2020-05-06 18:37:25 +0200

    fixy

--- tools/gen_venkovni_cfgd_ini_data.r
+++ tools/gen_venkovni_cfgd_ini_data.r
@@ -1,6 +1,9 @@
-setwd("/home/hdd/data/16_smod_paper_optim")
-files_ = list.files('data_raw/obs_data_venk_raw/', pattern = '*.csv', full.names = T)
-outs = 'test/'
+setwd("/home/hdd/data/16_smod_paper_optim/smoderp2d-optim-sens/")
+files_ = list.files('../data_raw/obs_data_venk_raw/', pattern = '*.csv', full.names = T)
+# jek pro testovani do tmp adresare
+# musi koncit lomitkem /
+outs = ''
+plocha = 16 #m2
 
 text_optim_cfg <- function(slope,rainfall,n,obs_data,model_out_path) {
   return (paste('[Params]
@@ -18,7 +21,7 @@ rows: ',n,'
 file: ',obs_data,'
 
 [Model]
-mod_file: ',model_out_path,'/point001.dat
+mod_file: ',model_out_path,'/point001.csv
 ', sep = ''))
 }
 
@@ -30,8 +33,8 @@ lu: -
 [shape atr]
 soil-atr: -
 lu-atr: -
-[srazka]
-file: model/indata/srazka.txt
+[rainfall]
+file: model/indata/rainfall.txt
 [time]
 # sec
 maxdt: 10
@@ -60,13 +63,13 @@ printtimes:
 
 text_cmd <- function(out_dir, model_ini, optim_cgs, mer_name){
   log = paste(outs,'logs/',paste(mer_name,'log',sep='.'),sep='')
-  return(paste('./optim.py -o',out_dir,'-m',model_ini,'-O',optim_cgs,'>',log))
+  return(paste('./optim.py -o',out_dir,'-m',model_ini,'-O',optim_cgs,'&>',log))
 }
 
 for (i.file in files_[1:3]) {
   r = read.table(i.file, header = TRUE, sep=',', dec='.')
   r = r[order(r$time_min),]
-  # plot(r$time_min, r$discharge.l_min.1.)
+  plot(r$time_min, r$discharge.l_min.1.)
   slope_prc = slope = r$sklon[1] # TODO prevest na procenta
   rainfall = r$rain[1]
   n = length(r$ID)
@@ -76,14 +79,14 @@ for (i.file in files_[1:3]) {
   model_out_path = paste(outs,'model/','out-',mer_name,sep='')
   model_ini_path =paste(outs,'model/',paste(mer_name,'ini',sep='.'),sep='')
   conf_path = paste(outs,'cfgs/',mer_name,'.cfgs',sep='')
-  cas = r$time_min # TODO prevest na spravne jednoty
-  val = r$discharge.l_min.1. # TODO prevest na spravne jednoty
+  cas = r$time_min 
+  val = (r$discharge.l_min.1./1000)/plocha*1000 # l/min -> mm/min
   # output path of optimiaztion
-  out_pat = paste(outs,'out-optim',mer_name,sep='-')
+  out_pat = paste(outs,'outs/out-',mer_name,sep='')
   
   # write cli run
   write(text_cmd(out_pat,model_ini_path,conf_path,mer_name), 
-        file = paste(outs,'runs',mer_name,sep='/'))
+        file = paste(outs,'runs_field_ds/',mer_name,sep=''))
   
   # write obs data
   write.table(x = data.frame(cas=cas, val=val),
