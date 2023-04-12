60b7372119c5f11bcc8042c5325aacb413ffb326
jerabekjak <jerabekjaku@gmail.com>
2023-04-12 14:38:16 +0200

    sens based on optim

--- sens.py
+++ sens.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python2
 
-from sens_ana.read_parser import read_parser
-from sens_ana.read_cfg import ReadConfig
+#from sens_ana.read_parser import read_parser
+#from diff_evol.read_parser import read_parser
+#from sens_ana.read_cfg import ReadConfig
+#from diff_evol.obs_data_handler import ObsData
 from sens_ana import SensAna
+from sens_ana.obs_data_handler import ObsData
+from diff_evol import DiffEvol
+from philip_optim import get_ks_s
+from diff_evol.read_parser import read_parser
 import os
 
 
 def main(pars):
     """ Perform one at the time optimization based on Morris, 1991 """
 
-    BF_sens = ReadConfig(pars.sens_conf)
+    #BF_sens = ReadConfig(pars.sens_conf)
+    OD = ObsData(pars.opt_conf)
     
     if not os.path.exists(pars.out_dir):
         os.makedirs(pars.out_dir)
 
-    SA = SensAna(pars=pars, cfgs=BF_sens)
+    SA = SensAna(pars=pars, cfgs=OD)
     SA.do_sa()
 
 
